7f32d6cfb100d6795a222ba380e662697515c2e7
Lucas Sloan <lucassloan@google.com>
2018-04-01 20:34:24 -0700

    Have the discriminator predict the labels as well as whether the image is generated.
    Not clear if it produces better images.

--- cifar_gan.py
+++ cifar_gan.py
@@ -8,18 +8,29 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-def _parse_function(filename):
+def parse_images(filename):
   image_string = tf.read_file(filename)
   image_decoded = tf.image.decode_png(image_string, channels=1)
   image_normalized = tf.image.convert_image_dtype(image_decoded, tf.float32)
-  image_cropped = tf.image.crop_to_bounding_box(image_normalized, 4, 4, 32, 32)
-  image_flipped = tf.image.random_flip_left_right(image_cropped)
+  image_flipped = tf.image.random_flip_left_right(image_normalized)
   return image_flipped
 
-def load_images(batch_size):
-    dataset = tf.data.Dataset.list_files("E:\\cifar10\\train\\*")
-    dataset = dataset.map(_parse_function, num_parallel_calls=4)
+def text_to_one_hot(text_label):
+    int_label = tf.string_to_number(text_label, out_type=tf.int32)
+    return tf.one_hot(int_label, 11)
+
+
+def load_images_and_labels(batch_size):
+    image_files_dataset = tf.data.Dataset.list_files("E:\\cifar10\\train\\*")
+    image_dataset = image_files_dataset.map(parse_images, num_parallel_calls=4)
+
+    label_lines_dataset = tf.data.TextLineDataset(["E:\\cifar10\\Train_cntk_text.txt"])
+    label_dataset = label_lines_dataset.map(text_to_one_hot)
+
+    dataset = tf.data.Dataset.zip((image_dataset, label_dataset))
+
     dataset = dataset.batch(batch_size)
+    # dataset = dataset.repeat()
     dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(10000))
     dataset = dataset.prefetch(batch_size)
     return dataset.make_one_shot_iterator()
@@ -60,27 +71,33 @@ def generator(z):
     fcb1 = tf.get_variable('fcb1', [1024], initializer=tf.constant_initializer(0.0))
     
     f1 = tf.nn.sigmoid(tf.matmul(z, fcW1) + fcb1)
-    f1 = tf.layers.batch_normalization(f1)
+    f1 = tf.layers.batch_normalization(f1, fused=True)
 
-    W2 = tf.get_variable('W2', [1024, 8 * 8 * 64], initializer=initializer)
-    b2 = tf.get_variable('b2', [8 * 8 * 64], initializer=tf.constant_initializer(0.0))
+    W2 = tf.get_variable('W2', [1024, 4 * 4 * 128], initializer=initializer)
+    b2 = tf.get_variable('b2', [4 * 4 * 128], initializer=tf.constant_initializer(0.0))
 
     f2 = tf.nn.sigmoid(tf.matmul(f1, W2) + b2)
-    f2 = tf.layers.batch_normalization(f2)
-    f2 = tf.reshape(f2, [-1, 8, 8, 64])
+    f2 = tf.layers.batch_normalization(f2, fused=True)
+    f2 = tf.reshape(f2, [-1, 4, 4, 128])
 
-    W_conv1 = tf.get_variable('W_conv1', [5, 5, 32, 64], initializer=initializer)
-    b_conv1 = tf.get_variable('b_conv1', [16, 16, 32], initializer=tf.constant_initializer(0.0))
+    W_conv1 = tf.get_variable('W_conv1', [5, 5, 64, 128], initializer=initializer)
+    b_conv1 = tf.get_variable('b_conv1', [8, 8, 64], initializer=tf.constant_initializer(0.0))
 
-    conv1 = tf.nn.sigmoid(tf.nn.conv2d_transpose(f2, W_conv1, [100, 16, 16, 32], [1, 2, 2, 1]) + b_conv1)
-    conv1 = tf.layers.batch_normalization(conv1)
+    conv1 = tf.nn.sigmoid(tf.nn.conv2d_transpose(f2, W_conv1, [100, 8, 8, 64], [1, 2, 2, 1]) + b_conv1)
+    conv1 = tf.layers.batch_normalization(conv1, fused=True)
 
-    W_conv2 = tf.get_variable('W_conv2', [5, 5, 1, 32], initializer=initializer)
-    b_conv2 = tf.get_variable('b_conv2', [32, 32, 1], initializer=tf.constant_initializer(0.0))
+    W_conv2 = tf.get_variable('W_conv2', [5, 5, 32, 64], initializer=initializer)
+    b_conv2 = tf.get_variable('b_conv2', [16, 16, 1], initializer=tf.constant_initializer(0.0))
 
-    conv2 = tf.nn.sigmoid(tf.nn.conv2d_transpose(conv1, W_conv2, [100, 32, 32, 1], [1, 2, 2, 1]) + b_conv2)
+    conv2 = tf.nn.sigmoid(tf.nn.conv2d_transpose(conv1, W_conv2, [100, 16, 16, 32], [1, 2, 2, 1]) + b_conv2)
+    conv2 = tf.layers.batch_normalization(conv2, fused=True)
 
-    return conv2
+    W_conv3 = tf.get_variable('W_conv3', [5, 5, 1, 32], initializer=initializer)
+    b_conv3 = tf.get_variable('b_conv3', [32, 32, 1], initializer=tf.constant_initializer(0.0))
+
+    conv3 = tf.nn.sigmoid(tf.nn.conv2d_transpose(conv2, W_conv3, [100, 32, 32, 1], [1, 2, 2, 1]) + b_conv3)
+
+    return conv3
 
 def minibatch_layer(input, num_kernels=5, kernel_dim=3):
     mb_fc_W = tf.get_variable('mb_fc_W', [input.get_shape()[1], num_kernels * kernel_dim], initializer=initializer)
@@ -107,19 +124,25 @@ def discriminator(x):
     h_conv2 = lrelu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
     h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*64])
+    W_conv3 = tf.get_variable('W_conv3', [5, 5, 64, 128], initializer=initializer)
+    b_conv3 = tf.get_variable('b_conv3', [128], initializer=tf.constant_initializer(0.0))
 
-    minibatch = minibatch_layer(h_pool2_flat, 10, 6)
+    h_conv3 = lrelu(tf.nn.conv2d(h_pool2, W_conv3, strides=[1, 1, 1, 1], padding='SAME') + b_conv3)
+    h_pool3 = tf.nn.max_pool(h_conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    W1 = tf.get_variable('W1', [8*8*64 + 10, 1024], initializer=initializer)
-    b1 = tf.get_variable('b1', [1024], initializer=tf.constant_initializer(0.0))
+    h_pool3_flat = tf.reshape(h_pool3, [-1, 4*4*128])
+
+    minibatch = minibatch_layer(h_pool3_flat, 10, 3)
+
+    # W1 = tf.get_variable('W1', [4*4*128 + 10, 2048], initializer=initializer)
+    # b1 = tf.get_variable('b1', [2048], initializer=tf.constant_initializer(0.0))
     
-    f1 = lrelu(tf.matmul(minibatch, W1) + b1)
+    # f1 = lrelu(tf.matmul(minibatch, W1) + b1)
 
-    W2 = tf.get_variable('W2', [1024, 1], initializer=initializer)
-    b2 = tf.get_variable('b2', [1], initializer=tf.constant_initializer(0.0))
+    W2 = tf.get_variable('W2', [4*4*128 + 10, 11], initializer=initializer)
+    b2 = tf.get_variable('b2', [11], initializer=tf.constant_initializer(0.0))
 
-    f2 = tf.nn.sigmoid(tf.matmul(f1, W2) + b2)
+    f2 = tf.matmul(minibatch, W2) + b2
 
     return f2
 
@@ -127,15 +150,23 @@ with tf.variable_scope('G'):
     z = tf.placeholder(tf.float32, [100, 25])
     G = generator(z)
 
-x = load_images(100).get_next()
+images_and_labels = load_images_and_labels(100).get_next()
+x = images_and_labels[0]
+yx = images_and_labels[1]
+yg = tf.reshape(tf.tile(tf.one_hot(10, 11), [100]), [100, 11])
+
 with tf.variable_scope('D'):
     Dx = discriminator(x)
 with tf.variable_scope('D', reuse=True):
     Dg = discriminator(G)
 
-loss_d = -tf.reduce_mean(tf.log(Dx) + tf.log(1.-Dg)) #This optimizes the discriminator.
-loss_g = -tf.reduce_mean(tf.log(Dg)) #This optimizes the generator.
+# loss_d = -tf.reduce_mean(tf.log(Dx) + tf.log(1.-Dg)) #This optimizes the discriminator.
+loss_d = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=yx, logits=Dx) + tf.nn.softmax_cross_entropy_with_logits(labels=yg, logits=Dg)) #This optimizes the discriminator.
+# loss_g = -tf.reduce_mean(tf.log(Dg)) #This optimizes the generator.
+loss_g = -tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=yg, logits=Dg)) #This optimizes the generator.
 
+correct_prediction = tf.equal(tf.argmax(Dg, 1), tf.argmax(yg, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 vars = tf.trainable_variables()
 for v in vars:
@@ -152,18 +183,21 @@ with tf.Session() as session:
 
     start_time = time.time()
     sample_directory = 'generated_images/cifar/{}'.format(start_time)
-    for step in range(100000):
+    for step in range(1000000):
         # update discriminator
         input_noise = np.random.rand(100, 25)
         loss_d_thingy, _ = session.run([loss_d, d_opt], {z: input_noise})
 
         # update generator
-        for i in range(10):
+        for i in range(15):
             input_noise = np.random.rand(100, 25)
             loss_g_thingy, _ = session.run([loss_g, g_opt], {z: input_noise})
 
         if step % 100 == 0:
+            input_noise = np.random.rand(100, 25)
+            train_accuracy = session.run([accuracy], {z: input_noise})
             print('{}: discriminator loss {:.8f}\tgenerator loss {:.8f}'.format(step, loss_d_thingy, loss_g_thingy))
+            print('train accuracy: {}'.format(train_accuracy))
 
         if step % 100 == 0:
             input_noise = np.random.rand(100, 25)
