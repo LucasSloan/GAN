c27ce9309e0501c6e8ea5dcb4741b219bc810663
Lucas Sloan <lucassloan@google.com>
2018-09-15 11:57:43 -0700

    Define the models using tf.layers, and add back batch normalization.

--- mnist_gan.py
+++ mnist_gan.py
@@ -9,7 +9,7 @@ import custom_layers
 
 def parse_images(filename):
   image_string = tf.read_file(filename)
-  image_decoded = tf.image.decode_png(image_string)
+  image_decoded = tf.image.decode_png(image_string, channels=1)
   image_resized = tf.image.resize_image_with_crop_or_pad(image_decoded, 32, 32)
   image_normalized = 2.0 * tf.image.convert_image_dtype(image_resized, tf.float32) - 1.0
   return image_normalized
@@ -29,78 +29,47 @@ def load_images(batch_size):
 initializer = tf.truncated_normal_initializer(stddev=0.02)
 
 
-def generator(z):
-    fcW1 = tf.get_variable('fcW1', [25, 1024], initializer=initializer)
-    fcb1 = tf.get_variable('fcb1', [1024], initializer=tf.constant_initializer(0.0))
-    
-    f1 = tf.nn.leaky_relu(tf.matmul(z, fcW1) + fcb1)
+def generator(z, training):
+    f1 = tf.layers.dense(z, 1024, tf.nn.leaky_relu)
+    f1 = tf.layers.batch_normalization(f1, training=training)
 
-    W2 = tf.get_variable('W2', [1024, 8 * 8 * 64], initializer=initializer)
-    b2 = tf.get_variable('b2', [8 * 8 * 64], initializer=tf.constant_initializer(0.0))
-
-    f2 = tf.nn.leaky_relu(tf.matmul(f1, W2) + b2)
+    f2 = tf.layers.dense(f1, 8 * 8 * 64, tf.nn.leaky_relu)
     f2 = tf.reshape(f2, [-1, 8, 8, 64])
+    f2 = tf.layers.batch_normalization(f2, training=training)
 
-    W_conv1 = tf.get_variable('W_conv1', [5, 5, 32, 64], initializer=initializer)
-    b_conv1 = tf.get_variable('b_conv1', [16, 16, 32], initializer=tf.constant_initializer(0.0))
+    conv1 = tf.layers.conv2d_transpose(f2, 32, [5, 5], strides=(2, 2), padding="same", activation=tf.nn.leaky_relu)
+    conv1 = tf.layers.batch_normalization(conv1, training=training)
 
-    conv1 = tf.nn.leaky_relu(tf.nn.conv2d_transpose(f2, W_conv1, [100, 16, 16, 32], [1, 2, 2, 1]) + b_conv1)
-
-    W_conv2 = tf.get_variable('W_conv2', [5, 5, 1, 32], initializer=initializer)
-    b_conv2 = tf.get_variable('b_conv2', [32, 32, 1], initializer=tf.constant_initializer(0.0))
-
-    conv2 = tf.nn.tanh(tf.nn.conv2d_transpose(conv1, W_conv2, [100, 32, 32, 1], [1, 2, 2, 1]) + b_conv2)
+    conv2 = tf.layers.conv2d_transpose(conv1, 1, [5, 5], strides=(2, 2), padding="same", activation=tf.nn.tanh)
 
     return conv2
 
-def discriminator(x):
-    W_conv1 = tf.get_variable('W_conv1', [5, 5, 1, 32], initializer=initializer)
-    b_conv1 = tf.get_variable('b_conv1', [32], initializer=tf.constant_initializer(0.0))
+def discriminator(x, training):
+    h_conv1 = tf.layers.conv2d(x, 32, [5, 5], strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
+    # h_conv1 = tf.layers.batch_normalization(h_conv1, training=training)
 
-    h_conv1 = tf.nn.leaky_relu(tf.nn.conv2d(x, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
-    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    h_conv2 = tf.layers.conv2d(h_conv1, 64, [5, 5], strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
+    h_conv2_flat = tf.reshape(h_conv2, [-1, 8*8*64])
+    # h_conv2_flat = tf.layers.batch_normalization(h_conv2_flat, training=training)
 
-    W_conv2 = tf.get_variable('W_conv2', [5, 5, 32, 64], initializer=initializer)
-    b_conv2 = tf.get_variable('b_conv2', [64], initializer=tf.constant_initializer(0.0))
+    f1 = tf.layers.dense(h_conv2_flat, 1024, tf.nn.leaky_relu)
+    # f1 = tf.layers.batch_normalization(f1, training=training)
 
-    h_conv2 = tf.nn.leaky_relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
-    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*64])
-
-    # minibatch = custom_layers.minibatch_layer(h_pool2_flat, initializer, 5, 3)
-
-    W1 = tf.get_variable('W1', [8*8*64, 1024], initializer=initializer)
-    b1 = tf.get_variable('b1', [1024], initializer=tf.constant_initializer(0.0))
-    
-    f1 = tf.nn.leaky_relu(tf.matmul(h_pool2_flat, W1) + b1)
-
-    W2 = tf.get_variable('W2', [1024, 1], initializer=initializer)
-    b2 = tf.get_variable('b2', [1], initializer=tf.constant_initializer(0.0))
-
-    f2 = tf.nn.sigmoid(tf.matmul(f1, W2) + b2)
+    f2 = tf.layers.dense(f1, 1, tf.nn.sigmoid)
 
     return f2
 
+training = tf.placeholder(tf.bool)
+
 with tf.variable_scope('G'):
-    z = tf.random_uniform([100, 25])
-    G = generator(z)
+    z = tf.random_uniform([100, 100])
+    G = generator(z, training)
 
 x = load_images(100).get_next()
 with tf.variable_scope('D'):
-    Dx = discriminator(x)
+    Dx = discriminator(x, training)
 with tf.variable_scope('D', reuse=True):
-    Dg = discriminator(G)
-
-def log(x):
-    """
-    Sometimes the discriminator outputs can reach values close to
-    (or even slightly less than) zero due to numerical rounding.
-    This just makes sure that we exclude those values so that we don't
-    end up with NaNs during optimization.
-    """
-    return tf.log(tf.maximum(x, 1e-5))
-
+    Dg = discriminator(G, training)
 
 loss_d = -tf.reduce_mean(tf.log(Dx) + tf.log(1.-Dg)) #This optimizes the discriminator.
 loss_g = -tf.reduce_mean(tf.log(Dg)) #This optimizes the generator.
@@ -125,10 +94,11 @@ with tf.Session() as session:
     for step in range(1, 100001):
         # update discriminator
         for _ in range(1):
-            loss_d_thingy, _ = session.run([loss_d, d_opt])
+            loss_d_thingy, _ = session.run([loss_d, d_opt], {training: True})
 
         # update generator
-        loss_g_thingy, _ = session.run([loss_g, g_opt])
+        for _ in range(1):
+            loss_g_thingy, _ = session.run([loss_g, g_opt], {training: True})
 
         if step % 100 == 0:
             print('{}: discriminator loss {:.8f}\tgenerator loss {:.8f}'.format(step, loss_d_thingy, loss_g_thingy))
@@ -139,8 +109,8 @@ with tf.Session() as session:
             previous_step_time = current_step_time
 
         if step % 1000 == 0:
-            gen_image = session.run(G)
-            real_image = session.run(x)
+            gen_image = session.run(G, {training: False})
+            real_image = session.run(x, {training: False})
             if not os.path.exists(sample_directory):
                 os.makedirs(sample_directory)
             save_images.save_images(np.reshape(gen_image, [100, 32, 32, 1]), [10, 10], sample_directory + '/{}gen.png'.format(step))
