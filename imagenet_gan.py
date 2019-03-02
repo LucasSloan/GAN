b94d5c98f78da50d6b438d13f925f74210111a9a
Lucas Sloan <lucas.sloan@gmail.com>
2019-03-01 22:01:57 -0800

    Update imagenet_gan to work with the latest revision of the GAN base class.

--- imagenet_gan.py
+++ imagenet_gan.py
@@ -4,6 +4,7 @@ from gan import GAN
 import resnet_blocks
 import ops
 import loader
+import os
 
 
 def parse_images(filename):
@@ -16,7 +17,7 @@ def parse_images(filename):
     image_nchw = tf.transpose(image_flipped, [2, 0, 1])
 
     filename = tf.reshape(filename, [1])
-    path_parts = tf.string_split(filename, "/")
+    path_parts = tf.string_split(filename, os.sep)
     dir = path_parts.values[-2]
     int_label = loader.text_to_index(dir)
     one_hot = loader.text_to_one_hot(dir, 1000)
@@ -25,8 +26,7 @@ def parse_images(filename):
 
 
 def load_imagenet(batch_size):
-    image_files_dataset = tf.data.Dataset.list_files(
-        "/home/lucas/training_data/imagenet/2012_train/*/*")
+    image_files_dataset = tf.data.Dataset.list_files("D:\\imagenet\\2012_train\\*\\*")
     image_dataset = image_files_dataset.map(parse_images, num_parallel_calls=4)
 
     dataset = image_dataset.apply(
@@ -37,7 +37,7 @@ def load_imagenet(batch_size):
     return dataset.make_one_shot_iterator()
 
 G_DIM = 64
-def resnet_generator(z):
+def resnet_generator(z, labels):
     with tf.variable_scope('generator'):
         linear = ops.linear(z, G_DIM * 8 * 4 * 4, use_sn=True)
         linear = tf.reshape(linear, [-1, G_DIM * 8, 4, 4])
@@ -60,7 +60,7 @@ def resnet_generator(z):
 
 
 D_DIM = 64
-def resnet_discriminator(x, reuse=False, use_sn=True, label_based_discriminator=False):
+def resnet_discriminator(x, labels, reuse=False, use_sn=True):
     with tf.variable_scope('discriminator', reuse=reuse):
         res1 = resnet_blocks.discriminator_residual_block(
             x, D_DIM, True, "res1", use_sn=use_sn, reuse=reuse) # 32x32
@@ -75,34 +75,27 @@ def resnet_discriminator(x, reuse=False, use_sn=True, label_based_discriminator=
 
         res5_flat = tf.reshape(res5, [-1, G_DIM * 8 * 4 * 4])
 
-        if label_based_discriminator:
-            f1_logit = ops.linear(res5_flat, 1001, scope="f1", use_sn=use_sn)
-            f1 = tf.nn.sigmoid(f1_logit)
-            return f1, f1_logit, None
-        else:
-            f1_logit = ops.linear(res5_flat, 1, scope="f1", use_sn=use_sn)
-            f1 = tf.nn.sigmoid(f1_logit)
-            return f1, f1_logit, None
+        f1_logit = ops.linear(res5_flat, 1, scope="f1", use_sn=use_sn)
+        f1 = tf.nn.sigmoid(f1_logit)
+        return f1, f1_logit, None
 
 
 class IMAGENET_GAN(GAN):
     def __init__(self, training_steps, batch_size, label_based_discriminator, output_real_images=False):
-        super().__init__(64, 64, "imagenet", training_steps, batch_size,
-                         label_based_discriminator, output_real_images=False)
+        super().__init__(64, 64, "imagenet", training_steps, batch_size, 1000, output_real_images=False)
 
-    def generator(self, z):
-        return resnet_generator(z)
+    def generator(self, z, labels):
+        return resnet_generator(z, labels)
 
-    def discriminator(self, x, label_based_discriminator):
-        Dx, Dx_logits, _ = resnet_discriminator(
-            x, reuse=False, use_sn=True, label_based_discriminator=label_based_discriminator)
+    def discriminator(self, x, labels):
+        Dx, Dx_logits, _ = resnet_discriminator(x, labels, reuse=False, use_sn=True)
         return Dx, Dx_logits
 
     def load_data(self, batch_size):
         images_and_labels = load_imagenet(batch_size).get_next()
         x = images_and_labels[0]
         x.set_shape([batch_size, 3, 64, 64])
-        yx = images_and_labels[1]
+        yx = images_and_labels[2]
         yg = tf.reshape(tf.tile(tf.one_hot(1000, 1001), [batch_size]), [batch_size, 1001])
 
         return x, yx, yg
