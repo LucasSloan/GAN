2e45ebe3cb679de803f30a6fe3ce77c589ce53ce
Lucas Sloan <lucas.sloan@gmail.com>
2019-03-27 20:11:21 -0700

    Experiment with more data augmentation for cifar.

--- cifar_loader.py
+++ cifar_loader.py
@@ -8,8 +8,13 @@ def parse_images(filename):
     image_string = tf.read_file(filename)
     image_decoded = tf.image.decode_png(image_string, channels=3)
     image_flipped = tf.image.random_flip_left_right(image_decoded)
+    image_hue = tf.image.random_hue(image_flipped, 0.08)
+    image_sat = tf.image.random_saturation(image_hue, 0.6, 1.6)
+    image_bright = tf.image.random_brightness(image_sat, 0.05)
+    image_contrast = tf.image.random_contrast(image_bright, 0.7, 1.3)
+
     image_normalized = 2.0 * \
-        tf.image.convert_image_dtype(image_flipped, tf.float32) - 1.0
+        tf.image.convert_image_dtype(image_contrast, tf.float32) - 1.0
     image_nchw = tf.transpose(image_normalized, [2, 0, 1])
 
     filename = tf.reshape(filename, [1])
